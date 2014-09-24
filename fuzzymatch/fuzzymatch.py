f162bbd0084851dbc0aa3033a10a97d3420fcd97
Milos Curcic <milos.curcic@bazaarvoice.com>
2014-09-24 16:37:23 -0500

    Changing the threshold and printout format

--- fuzzymatch/fuzzymatch.py
+++ fuzzymatch/fuzzymatch.py
@@ -1,6 +1,6 @@
 from fuzzywuzzy import process
 
-FUZZY_THRESHOLD = 90
+FUZZY_THRESHOLD = 70
 MATCHES_COUNT = 3
 
 INPUT_FILE = "test-input.dat"
@@ -22,7 +22,7 @@ def main():
 		matched_list = process.extract(name, corpus_list, limit = MATCHES_COUNT)
 		# filter the list of matches to be above the threshold
 		filtered_matched_list = filter(lambda t: t[1] >= FUZZY_THRESHOLD, matched_list)
-		print filtered_matched_list
+		print name, "-->", filtered_matched_list
 
 
 if __name__ == "__main__":
