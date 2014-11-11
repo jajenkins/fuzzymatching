309747f1701c5438b3747d13351369b2e67aa72e
Milos Curcic <milos.curcic@bazaarvoice.com>
2014-11-11 15:48:07 -0600

    Adding printing to output file

--- fuzzymatch/fuzzymatch.py
+++ fuzzymatch/fuzzymatch.py
@@ -6,12 +6,15 @@ MATCHES_COUNT = 3
 
 INPUT_FILE = "test-input.dat"
 CORPUS_FILE = "test-corpus.dat"
+OUTPUT_FILE = "test-output.dat"
 
 def main():
 	# open input file
 	input_file = codecs.open(INPUT_FILE, "r", "utf-8")
 	# read lines from input file and strip the new line character at the end
 	input_list = [x.strip() for x in input_file.readlines()]
+
+	output_file = codecs.open(OUTPUT_FILE, 'w', "utf-8")
 
 	# open input file
 	corpus_file = codecs.open(CORPUS_FILE, "r", "utf-8")
@@ -24,8 +27,10 @@ def main():
 		# filter the list of matches to be above the threshold
 		filtered_matched_list = filter(lambda t: t[1] >= FUZZY_THRESHOLD, matched_list)
 
-		print name + ",", ", ".join(list(map(lambda t: t[0], filtered_matched_list)))
+		output_str = '%s' % name + ", " + ", ".join(list(map(lambda t: '(%s, %s)' % (t[0], t[1]), filtered_matched_list))) + "\n"
+		output_file.write(output_str)
 
+	output_file.close()
 
 if __name__ == "__main__":
     main()
